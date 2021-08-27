3e3810d56a9e17127b3be993a968fcfaa1930e02
Arthur-Rocha-Lima <2001arthur.rocha@gmail.com>
2021-08-26 21:41:04 -0300

    Finalização imagem docker

---
+++ projetoPHP/database/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "db";
+$username = "root";
+$password = "arthur123";
+$banco = "banco-mysql";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $banco);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$res = $conn->query("SELECT * from pacientes");
+
+echo "<pre>";
+print_r($res->fetch_all());
